f45e5c1dea173b4e84bbb189da436f42b3c3919b
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-28 21:38:23 +0100

    connect to database using env + a POST request to add meetups added

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,36 @@
 // /api/new-meetup
 // POST /api/new-meetup
+import { MongoClient } from 'mongodb';
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
+
+    try {
+      const client = await MongoClient.connect(
+        `mongodb+srv://admin-abay:${process.env.DB_ACCESS_KEY}@cluster0.390sm.mongodb.net/meetups?retryWrites=true&w=majority`
+      );
+
+      console.log('Connected to database!');
+
+      const db = client.db();
+
+      const meetupsCollection = db.collection('meetups');
+
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: 'Meetup inserted!' });
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1); //Exit the app is there are errors
+    }
   }
 }
+
+export default handler;
