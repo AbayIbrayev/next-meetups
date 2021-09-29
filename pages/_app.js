0bf3d4165bb138e20a1fbca77d980191acf0103a
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-29 16:39:57 +0100

    added a progres bar on route change

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,21 @@ import Layout from '../components/layout/Layout';
 import { Fragment } from 'react';
 import Head from 'next/head';
 
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import 'nprogress/nprogress.css';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
